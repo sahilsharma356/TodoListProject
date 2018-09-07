2458cd294f6db66ee872d65fa7aa9c6bb958f248
Sahil Sharma <sahilsharma19990816@gmail.com>
2018-09-06 21:32:42 -0400

    commit2

---
+++ Assets/js/todos.js
@@ -0,0 +1,24 @@
+// check off todos by clicking on it 
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed"); 
+}); 
+
+//click on X to delete point 
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	}); 
+	event.stopPropogation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val(); 
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
